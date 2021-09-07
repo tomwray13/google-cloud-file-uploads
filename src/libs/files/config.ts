10c9454919b54612119a12fe38fe1611ee5bf887
Tom Ray <tomwray13@gmail.com>
2021-09-07 12:04:55 +0100

    Configure Google Cloud Storage

---
+++ src/libs/files/config.ts
@@ -0,0 +1,11 @@
+import { Storage } from '@google-cloud/storage'
+
+const storage = new Storage({
+  projectId: process.env.GCP_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GCP_CLIENT_EMAIL,
+    private_key: process.env.GCP_PRIVATE_KEY ? process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n') : ''
+  }
+});
+
+export const bucket = storage.bucket(process.env.GCP_BUCKET_ID || "")
